f51d26a67653c22015948311b4e6b8264a227013
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-08-14 16:13:26 -0400

    implements curveVal function that squares the input value, softening the lower part of the curve

--- arduino-breathelight/arduino-breathelight.ino
+++ arduino-breathelight/arduino-breathelight.ino
@@ -1,14 +1,26 @@
+// rampTime is the length of one cycle in milliseconds
+int rampTime=1000;
+
+
+int curPos=0;
+int curDirection=1;
+int delayAmt=rampTime/255;
+int curLevel=0;
 void setup() {
-  // put your setup code here, to run once:
 pinMode(13,OUTPUT);
 }
 
+int curveVal(int value){
+  return value*value/255;
+}
+
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
- analogWrite(13, 100); // sets the digital pin 13 on
-delay(1000);
-analogWrite(13,255);
-delay(250);
+curLevel=curveVal(curPos);
+analogWrite(13, curLevel);
+delay(delayAmt);
+curPos=curPos+curDirection;
+if (curPos==255){curDirection=-1;}
+if (curPos==0){curDirection=1;}
 }
