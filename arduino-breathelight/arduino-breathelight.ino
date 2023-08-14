8111ce383a5ddbb34b71c1869c1a780f0003853e
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-08-14 16:02:03 -0400

    tests LED pin to confirm PWM is working

--- arduino-breathelight/arduino-breathelight.ino
+++ arduino-breathelight/arduino-breathelight.ino
@@ -1,9 +1,14 @@
 void setup() {
   // put your setup code here, to run once:
-
+pinMode(13,OUTPUT);
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+ analogWrite(13, 100); // sets the digital pin 13 on
+delay(1000);
+analogWrite(13,255);
+delay(250);
 }
