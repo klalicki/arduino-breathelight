f5807a1821bddd7766b7b51dbbd8f5c11f75960f
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-08-14 17:49:49 -0400

    implements cosine curve function

--- arduino-breathelight/arduino-breathelight.ino
+++ arduino-breathelight/arduino-breathelight.ino
@@ -1,30 +1,30 @@
 // rampTime is the length of one cycle in milliseconds
-int rampTime=10000;
+int rampTime = 20000;
 
 
-int curPos=0;
-int delayAmt=rampTime/360;
+int curPos = 0;
+int delayAmt = rampTime / 360;
 
 
 // Set 'TOP' for PWM resolution.  Assumes 16 MHz clock.
 // un-comment one of the choices
-const unsigned int TOP = 0xFFFF; // 16-bit resolution.   244 Hz PWM
+const unsigned int TOP = 0xFFFF;  // 16-bit resolution.   244 Hz PWM
 // const unsigned int TOP = 0x7FFF; // 15-bit resolution.   488 Hz PWM
 // const unsigned int TOP = 0x3FFF; // 14-bit resolution.   976 Hz PWM
 // const unsigned int TOP = 0x1FFF; // 13-bit resolution.  1953 Hz PWM
 // const unsigned int TOP = 0x0FFF; // 12-bit resolution.  3906 Hz PWM
 // const unsigned int TOP = 0x07FF; // 11-bit resolution.  7812 Hz PWM
-  //  const unsigned int TOP = 0x03FF; // 10-bit resolution. 15624 Hz PWM
+//  const unsigned int TOP = 0x03FF; // 10-bit resolution. 15624 Hz PWM
 
-float degToRad(float degree){
-  return degree* (M_PI / 180.0);
+float degToRad(float degree) {
+  return degree * (M_PI / 180.0);
 }
 
 void PWM16Begin() {
   // Stop Timer/Counter1
   TCCR1A = 0;  // Timer/Counter1 Control Register A
   TCCR1B = 0;  // Timer/Counter1 Control Register B
-  TIMSK1 = 0;   // Timer/Counter1 Interrupt Mask Register
+  TIMSK1 = 0;  // Timer/Counter1 Interrupt Mask Register
   TIFR1 = 0;   // Timer/Counter1 Interrupt Flag Register
   ICR1 = TOP;
   OCR1A = 0;  // Default to 0% PWM
@@ -35,7 +35,7 @@ void PWM16Begin() {
 
   // Set to Timer/Counter1 to Waveform Generation Mode 14: Fast PWM with TOP set by ICR1
   TCCR1A |= (1 << WGM11);
-  TCCR1B |= (1 << WGM13) | (1 << WGM12) ;
+  TCCR1B |= (1 << WGM13) | (1 << WGM12);
 }
 
 void PWM16EnableA() {
@@ -62,31 +62,29 @@ inline void PWM16B(unsigned int PWMValue) {
 void setup() {
   Serial.begin(9600);
   PWM16Begin();
-  
+
   // On the Arduino UNO T1A is Pin 9 and T1B is Pin 10
-  
-//  PWM16A(0);  // Set initial PWM value for Pin 9
-//  PWM16EnableA();  // Turn PWM on for Pin 9
 
-  PWM16B(0);  // Set initial PWM value for Pin 10
+  //  PWM16A(0);  // Set initial PWM value for Pin 9
+  //  PWM16EnableA();  // Turn PWM on for Pin 9
+
+  PWM16B(0);       // Set initial PWM value for Pin 10
   PWM16EnableB();  // Turn PWM on for Pin 10
-
 }
 
-int curveVal(float value){
-  float normalizedCos= (-cos(value)+1)/2;
-  return normalizedCos*255*255;
+int curveVal(float value) {
+  float normalizedCos = (-cos(value) + 1) / 2;
+  return normalizedCos * 255 * 255;
 }
 
 
 
 void loop() {
-  float curRad= degToRad(curPos);
 
-PWM16B(curveVal(curRad));
-delay(delayAmt);
-curPos=curPos+1;
-if (curPos==360){curPos=0;}
+  float curRad = degToRad(curPos);
 
+  PWM16B(curveVal(curRad));
+  delay(delayAmt);
+  curPos = curPos + 1;
+  if (curPos == 360) { curPos = 0; }
 }
-
